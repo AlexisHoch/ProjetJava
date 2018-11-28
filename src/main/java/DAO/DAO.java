52dcd01b5b7803499461f4b311e67f437cc901ec
Alexis <pedago@pcalb-mm0416.univ-jfc.fr>
2018-11-28 16:51:32 +0100

    Controleur

--- src/main/java/DAO/DAO.java
+++ src/main/java/DAO/DAO.java
@@ -5,6 +5,13 @@
  */
 package DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.DataSource;
+
 /**
  *
  * @author pedago
@@ -12,8 +19,49 @@ package DAO;
 public class DAO {
     
     
+    	private final DataSource myDataSource;
+
+	/**
+	 *
+	 * @param dataSource la source de données à utiliser
+	 */
+	public DAO(DataSource dataSource) {
+		this.myDataSource = dataSource;
+	}
     
     
-    
-    
+        
+ 
+        public void newAccount(String disc, int zip) throws SQLException{
+            
+            String sql = "INSERT INTO Customer(CUSTOMER_ID, DISCOUNT_CODE, ZIP) VALUES (?, ?, ?)";
+                    //--------------------------------ID NAME EMAIL
+            
+            try(Connection myConnection = myDataSource.getConnection();
+                    PreparedStatement customerStatement = myConnection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+                    
+                    customerStatement.setInt(1, customerStatement.getGeneratedKeys().getInt("ID"));
+                    customerStatement.setString(2, disc);
+                    customerStatement.setInt(3, zip);
+                    
+                    customerStatement.executeUpdate();
+                    }
+                
+            }
+        
+        public int numberOfCustomers() throws SQLException {
+		int result = 0;
+
+		String sql = "SELECT COUNT(*) AS NUMBER FROM Customer";
+		try (Connection connection = myDataSource.getConnection();
+		     Statement stmt = connection.createStatement()) {
+			ResultSet rs = stmt.executeQuery(sql);
+			if (rs.next()) {
+				result = rs.getInt("NUMBER");
+			}
+		}
+		return result;
+	}
+            
+                    
 }
---
+++ src/test/java/DAOTest.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import DAO.DAO;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.hsqldb.cmdline.SqlFile;
+import org.hsqldb.cmdline.SqlToolError;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pedago
+ */
+public class DAOTest {
+    private static DataSource myDataSource;
+    private static Connection myConnection ;
+	
+    private DAO dao;
+	public  void setUp() throws IOException, SqlToolError, SQLException {
+		// On crée la connection vers la base de test "in memory"
+		myDataSource = getDataSource();
+		myConnection = myDataSource.getConnection();
+		// On crée le schema de la base de test
+		executeSQLScript(myConnection, "schema.sql");
+		// On y met des données
+		executeSQLScript(myConnection, "smalltestdata.sql");		
+
+            	dao = new DAO(myDataSource);
+    }
+    private void executeSQLScript(Connection connexion, String filename)  throws IOException, SqlToolError, SQLException {
+		// On initialise la base avec le contenu d'un fichier de test
+		String sqlFilePath = DAOTest.class.getResource(filename).getFile();
+		SqlFile sqlFile = new SqlFile(new File(sqlFilePath));
+
+		sqlFile.setConnection(connexion);
+		sqlFile.execute();
+		sqlFile.closeReader();		
+	}
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Test
+    public void addCustomerTest() throws SQLException{
+        dao.newAccount("L", 48100);
+        assertEquals(dao.numberOfCustomers(), 1);
+    }
+    
+    @After
+    public void tearDown() throws SQLException {
+        	myConnection.close(); // La base de données de test est détruite ici
+             	dao = null; // Pas vraiment utile
+    }
+
+
+public static DataSource getDataSource() {
+		org.hsqldb.jdbc.JDBCDataSource ds = new org.hsqldb.jdbc.JDBCDataSource();
+		ds.setDatabase("jdbc:hsqldb:mem:testcase;shutdown=true");
+		ds.setUser("sa");
+		ds.setPassword("sa");
+		return ds;
+
+}
+
+}
