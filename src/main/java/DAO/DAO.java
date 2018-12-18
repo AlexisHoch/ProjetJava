849b16fab11d877c17930a9c64bb8c2cc52e85c6
AlexisHoch <37694448+AlexisHoch@users.noreply.github.com>
2018-12-18 13:43:18 +0100

    -prix

--- src/main/java/DAO/DAO.java
+++ src/main/java/DAO/DAO.java
@@ -55,40 +55,34 @@ public class DAO {
             }
         
         
-        public void newInvoice(int customerID) throws SQLException{
-            
-            String sql = "INSERT INTO Invoice(ID, CustomerID) VALUES ( (SELECT MAX(ID)+1 FROM Invoice), ?)";
-            try(Connection myConnection = myDataSource.getConnection();
-                    PreparedStatement customerStatement = myConnection.prepareStatement(sql)){
-                    
-                    customerStatement.setInt(1, customerID);
-                    System.out.println(customerStatement.toString());
-                    customerStatement.executeUpdate();
-                    }
-            
-        }
-        
-        
-               
-        public void addItemToInvoice(int invoiceID, int productID, int quantity) throws SQLException{
-            String sql = "INSERT INTO Item(Item, InvoiceID, ProductID, Quantity) VALUES ( (SELECT MAX(ID)+1 FROM Invoice), ?, ?, ?)";
-            String cost = "SELECT Purchase_cost FROM PRODUCT WHERE PRODUCT_ID = ?";
-            int totalCostItem;
-            try(Connection myConnection = myDataSource.getConnection();
-                    
-                    PreparedStatement customerStatement = myConnection.prepareStatement(sql)){
                 
-                    
-                    customerStatement.setInt(1, invoiceID);
-                    customerStatement.setInt(2, productID);
-                    customerStatement.setInt(3, quantity);
-                    System.out.println(customerStatement.toString());
-                    customerStatement.executeUpdate();
-                    }
-            
+        public Customer findCustomer(int id) throws SQLException {
+            Customer c = null;
+            String sql = "SELECT * FROM Customer WHERE email = ?";
+            try (Connection myConnection = myDataSource.getConnection();
+                PreparedStatement statement = myConnection.prepareStatement(sql)) {
+                statement.setInt(1, id); // On fixe le 1° paramètre de la requête
+            try (ResultSet rs = statement.executeQuery()) {
+                if (rs.next()) {
+                    String email = rs.getString("EMAIL");
+                    String dc = rs.getString("DISCOUNT_CODE");
+                    int zip = rs.getInt("ZIP");
+                    String name = rs.getString("NAME");
+                    String adress = rs.getString("ADDRESSLINE1");
+                    String adress2 = rs.getString("ADDRESSLINE2");
+                    String city = rs.getString("CITY");
+                    String state = rs.getString("STATE");
+                    String phone = rs.getString("PHONE");
+                    String fax = rs.getString("FAX");
+                    int credit = rs.getInt("CREDIT_LIMIT");
+                    c = new Customer(id, dc, zip, name, adress, adress2, city, state, phone, fax, email, credit);
+                }
+            }
         }
-             
+        return c;
+    }
         
+                
         public int numberOfCustomers() throws SQLException {
 		int result = 0;
 
@@ -152,43 +146,129 @@ public class DAO {
 	}
         
         
-        public Customer findCustomer(int id) throws SQLException {
-            Customer c = null;
-            String sql = "SELECT * FROM Customer WHERE email = ?";
-            try (Connection myConnection = myDataSource.getConnection();
-                PreparedStatement statement = myConnection.prepareStatement(sql)) {
-                statement.setInt(1, id); // On fixe le 1° paramètre de la requête
-            try (ResultSet rs = statement.executeQuery()) {
-                if (rs.next()) {
-                    String email = rs.getString("EMAIL");
-                    String dc = rs.getString("DISCOUNT_CODE");
-                    int zip = rs.getInt("ZIP");
-                    String name = rs.getString("NAME");
-                    String adress = rs.getString("ADDRESSLINE1");
-                    String adress2 = rs.getString("ADDRESSLINE2");
-                    String city = rs.getString("CITY");
-                    String state = rs.getString("STATE");
-                    String phone = rs.getString("PHONE");
-                    String fax = rs.getString("FAX");
-                    int credit = rs.getInt("CREDIT_LIMIT");
-                    c = new Customer(id, dc, zip, name, adress, adress2, city, state, phone, fax, email, credit);
-                }
+        public int numberOfInvoices() throws SQLException{
+            
+            int results=0;
+
+		String sql = "SELECT COUNT(*) AS NUMBER FROM INVOICE";
+		try (Connection connection = myDataSource.getConnection();
+		     Statement stmt = connection.createStatement()) {
+			ResultSet rs = stmt.executeQuery(sql);
+			if (rs.next()) {
+				results = rs.getInt("NUMBER");
+			}
+		}
+		return results;
+            
+        }
+        
+        
+        public void newInvoice(int customerID) throws SQLException{
+            
+            String sql = "INSERT INTO Invoice(ID, CustomerID) VALUES ( (SELECT MAX(ID)+1 FROM Invoice), ?)";
+            try(Connection myConnection = myDataSource.getConnection();
+                    PreparedStatement customerStatement = myConnection.prepareStatement(sql)){
+                    
+                    customerStatement.setInt(1, customerID);
+                    System.out.println(customerStatement.toString());
+                    customerStatement.executeUpdate();
+                    }
+            
+        }
+        
+        
+        /* Non fonctionnelle encore
+        public void deleteItem(int itemId) throws SQLException {
+               int invoiceID;
+              try (Connection connection = myDataSource.getConnection();
+		     PreparedStatement stmt = connection.prepareStatement("SELECT * FROM ITEM WHERE ITEM = ?")) {
+			ResultSet rs = stmt.executeQuery();
+                        
+			if (rs.next()) {
+				invoiceID = rs.getInt("INVOICEID");
+			}
+		}
+               
+               
+		// Une requête SQL paramétrée
+		String sql = "DELETE FROM ITEM WHERE ITEM = ?";
+		try (   Connection connection = myDataSource.getConnection();
+			PreparedStatement stmt = connection.prepareStatement(sql)
+                ) {
+                        // Définir la valeur du paramètre
+			stmt.setInt(1, itemId);
+			
+			stmt.executeUpdate();
+
+		}  catch (SQLException ex) {
+			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
+			
+		}
+                
+                
+            }
+        */
+        
+               
+        public void addItemToInvoice(int invoiceID, int productID, int quantity) throws SQLException{
+            String sql = "INSERT INTO Item(Item, InvoiceID, ProductID, Quantity, Cost) VALUES ( (SELECT MAX(ID)+1 FROM Invoice), ?, ?, ?, ?)";
+            String cost = "SELECT Purchase_cost FROM PRODUCT WHERE PRODUCT_ID = ?";
+            int totalCostItem;
+            try(Connection myConnection = myDataSource.getConnection();
+                    
+                    //On, va chercher le cout du produit
+                    PreparedStatement customerStatement = myConnection.prepareStatement(sql)){
+                        try(Connection myConnection1 = myDataSource.getConnection();
+
+                        PreparedStatement customerStatement1 = myConnection.prepareStatement(cost)){
+
+
+                        customerStatement1.setInt(1, productID);
+                        System.out.println(customerStatement.toString());
+                        ResultSet res = customerStatement1.executeQuery();
+                        res.next();
+                        totalCostItem=res.getInt(1);
+                        }
+                    totalCostItem*=quantity;
+                    customerStatement.setInt(1, invoiceID);
+                    customerStatement.setInt(2, productID);
+                    customerStatement.setInt(3, quantity);
+                    customerStatement.setInt(4, totalCostItem);
+                    System.out.println(customerStatement.toString());
+                    customerStatement.executeUpdate();
+                    
+ 
             }
         }
-        return c;
-    }
         
+        public int numberOfItems() throws SQLException {
+		int result = 0;
+
+		String sql = "SELECT COUNT(*) AS NUMBER FROM Item";
+		try (Connection connection = myDataSource.getConnection();
+		     Statement stmt = connection.createStatement()) {
+			ResultSet rs = stmt.executeQuery(sql);
+			if (rs.next()) {
+				result = rs.getInt("NUMBER");
+			}
+		}
+		return result;
+	}
         
-    public void newProduct(String disc, int zip) throws SQLException{
+             
+
+
+        
+    public void newProduct(int manufacturerID, String productCode) throws SQLException{
             
-            String sql = "INSERT INTO Customer(CUSTOMER_ID, DISCOUNT_CODE, ZIP) VALUES ( (SELECT MAX(CUSTOMER_ID)+1 FROM CUSTOMER), ?, ?)";
+            String sql = "INSERT INTO Product(PRODUCT_ID, MANUFACTURER_ID, PRODUCT_CODE) VALUES ( (SELECT MAX(CUSTOMER_ID)+1 FROM CUSTOMER), ?, ?)";
                     //--------------------------------ID NAME EMAIL
             
             try(Connection myConnection = myDataSource.getConnection();
                     PreparedStatement customerStatement = myConnection.prepareStatement(sql)){
                     
-                    customerStatement.setString(1, disc);
-                    customerStatement.setInt(2, zip);
+                    customerStatement.setInt(1, manufacturerID);
+                    customerStatement.setString(2, productCode);
                     System.out.println(customerStatement.toString());
                     customerStatement.executeUpdate();
                     }
